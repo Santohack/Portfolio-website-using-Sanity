4460325866a865902fa6ae3e06aacc4cb05c41bc
santohack <santohack238@gmail.com>
2022-10-18 19:14:15 +0530

    frontend

---
+++ next/sanityfront/pages/index.js
@@ -0,0 +1,7 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return <div className="home text-red-400">this is hone.</div>;
+}
